e9d0ede3d4c6d781284619deef05be123b4fe06e
Sergiu Canschi <info@webdesign7.co.uk>
2023-06-30 13:00:59 +0100

    PHP CS

--- src/app/Http/Controllers/Games.php
+++ src/app/Http/Controllers/Games.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class Games extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('welcome');
     }
 }
