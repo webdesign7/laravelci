54a8320a5ba80de520da7866196d1e6a226193d2
Web Developer London <info@webdesign7.co.uk>
2024-09-11 20:37:52 +0300

    Update Games.php
    Test

--- src/app/Http/Controllers/Games.php
+++ src/app/Http/Controllers/Games.php
@@ -12,4 +12,15 @@ class Games extends Controller
     public function index(){
         return view('welcome');
     }
+
+    public function another_function_very_long(){
+        $x = 0;    
+        if ($x = 1) {
+        return view('welcome');
+        } else {
+        return view('welcome');
+        }
+        
+        return view('welcome');
+    }
 }
